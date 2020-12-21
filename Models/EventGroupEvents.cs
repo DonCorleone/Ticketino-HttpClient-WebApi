62d8323b8ffac5a64388cb85311ad4af161ee2b6
Linus <vitocorleone77@gmail.com>
2020-12-21 22:18:04 +0100

    EventGroupEvents BSON

--- Models/EventGroupEvents.cs
+++ Models/EventGroupEvents.cs
@@ -1,9 +1,14 @@
 using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Kinderkultur_TicketinoClient.Models
 {
     public class EventGroupEvents
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string IddB { get; set; }
         public int eventGroupId { get; set; } 
         public List<EventOverview> events { get; set; }
     }
